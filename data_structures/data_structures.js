9c30519b5626d9cf505426dad9115a28eb0c2f70
Amber Hunt <amberhunt955@gmail.com>
2023-02-20 22:40:48 -0500

    data structures self study

--- data_structures/data_structures.js
+++ data_structures/data_structures.js
@@ -1,61 +1,222 @@
-// Node Basic Class
-// The export keyword makes this class available outside of this module (file)
-export class Node {
-    constructor(data) {
+//& Class study
+
+// // Node Basic Class
+// // The export keyword makes this class available outside of this module (file)
+// export class Node {
+//     constructor(data) {
+//         this.data = data;
+//     }
+//     next = null;
+// }
+
+// // Linked List Class
+// export class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+
+//     getHeadNode() {
+//         return this.head;
+//     }
+
+//     clear() {
+//         this.head = null;
+//     }
+    
+//     traverse() {
+//         let walker = this.head; // first node in the list
+//         while(walker.next) { // while walker.next != null is true
+//             console.log(walker.data)
+//             walker = walker.next; // moving to the next Node
+//         }
+//     } 
+
+//     getLast() {
+//     }
+
+//     appendNodeEnd() {
+//     }
+// }
+
+// export class TextNode {
+//     constructor(data) {
+//         this.data = data;
+//         this.length = length;
+//     }
+// }
+
+// export class ElementNode {
+//     constructor(name) {
+//         this.elementName = name;
+//         this.textContent = null;
+//         this.outerHTML = null;
+//     }
+
+//     setTextContent(text) {
+//         this.textContent = text;
+//         this.outerHTML = this.#setOuterHTML();
+//     }
+
+//     #setOuterHTML() {
+//         return `<${this.elementName}>${this.textContent}<${this.elementName}>`
+//     }
+// }
+
+//& Self study
+
+class Node {
+    constructor(data, next = null) {
         this.data = data;
+        this.next = next;
     }
-    next = null;
 }
 
-// Linked List Class
-export class LinkedList {
+class LinkedList {
     constructor() {
         this.head = null;
+        this.size = 0;
     }
 
-    getHeadNode() {
-        return this.head;
+    //* Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++;
     }
 
-    clear() {
-        this.head = null;
-    }
-    
-    traverse() {
-        let walker = this.head; // first node in the list
-        while(walker.next) { // while walker.next != null is true
-            console.log(walker.data)
-            walker = walker.next; // moving to the next Node
+    //* Insert last node
+    insertLast(data) {
+        // Create the node to be inserted at the end of the linked list
+        let node = new Node(data);
+        // Initialize the variable currentNode
+        let currentNode;
+
+        // If linked list is empty, then then make node the head node
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            // We need to loop through the nodes starting at the head
+            currentNode = this.head;
+            
+            while(currentNode.next) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = node;
         }
-    } 
 
-    getLast() {
+        // Increase the size
+        this.size++;
     }
 
-    appendNodeEnd() {
+    //* Insert at index
+    insertAt(data, index) {
+        // If index is out of range
+        if (index > 0 && index > this.size) {
+            return;
+        }
+
+        // If first index
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            this.size++;
+            return;
+        }
+
+        // Create the node to be inserted
+        const node = new Node(data);
+        // Initialize two new variables
+        let currentNode, previousNode;
+
+        // Set current node to first
+        currentNode = this.head;
+        let count = 0; // Now we need to loop through the nodes
+
+        while (count < index) {
+            previousNode = currentNode; // Node before index
+            count++;
+            currentNode = currentNode.next; // Node after the index
+        }
+
+        node.next = currentNode;
+        previousNode.next = node;
+        this.size++;
+    }
+
+    //* Get at index
+    getAt(index) {
+        let currentNode = this.head;
+        let count = 0;
+        let dataAtIndex;
+
+        while (currentNode) {
+            // this will tell us which node we need
+            if (count === index) {
+                dataAtIndex = currentNode.data;
+                console.log(dataAtIndex);
+            }
+            count++;
+            currentNode = currentNode.next;
+        }
+
+        return dataAtIndex;
+    }
+
+    //* Remove at index
+    removeAt (index) {
+        // If out of range, then just return
+        if (index > 0 && index > this.size - 1) {
+            return;
+        }
+
+        let currentNode = this.head;
+        let previousNode;
+        let count = 0;
+
+        if (index === 0) { // removing the first node
+            this.head = currentNode.next; 
+            this.size--;
+        } else {
+            while(count < index) {
+                count++;
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+
+            previousNode.next = currentNode.next;
+            this.size--;
+        }
+    }
+
+    //* Clear list
+    clearList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //* Print list data
+    printListData() {
+        let currentNode = this.head;
+
+        while(currentNode) {
+            console.log(currentNode.data);
+            currentNode = currentNode.next;
+        }
     }
 }
 
-export class TextNode {
-    constructor(data) {
-        this.data = data;
-        this.length = length;
-    }
-}
-
-export class ElementNode {
-    constructor(name) {
-        this.elementName = name;
-        this.textContent = null;
-        this.outerHTML = null;
-    }
-
-    setTextContent(text) {
-        this.textContent = text;
-        this.outerHTML = this.#setOuterHTML();
-    }
-
-    #setOuterHTML() {
-        return `<${this.elementName}>${this.textContent}<${this.elementName}>`
-    }
-}
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+ll.insertAt(500, 0);
+console.log(ll);
+ll.printListData();
+console.log("Get at 2");
+ll.getAt(2);
+console.log("Remove at 2");
+ll.removeAt(2);
+ll.printListData();
+console.log(ll);
+ll.clearList();
+console.log(ll);
--- data_structures/index.js
+++ data_structures/index.js
@@ -1,65 +1,67 @@
-import { Node, LinkedList, TextNode, ElementNode } from "./data_structures.js";
+//& Class study
 
-// Create three instances of nodes
-const node1 = new Node(4);
-const node2 = new Node(7);
-const node3 = new Node(12);
+// import { Node, LinkedList, TextNode, ElementNode } from "./data_structures.js";
 
-console.log(node1);
-console.log(node2);
-console.log(node3);
+// // Create three instances of nodes
+// const node1 = new Node(4);
+// const node2 = new Node(7);
+// const node3 = new Node(12);
 
-// Create a new instance of the Linked List
-const myLinkedList = new LinkedList();
+// console.log(node1);
+// console.log(node2);
+// console.log(node3);
 
-console.log(myLinkedList);
+// // Create a new instance of the Linked List
+// const myLinkedList = new LinkedList();
 
-// Let's link our nodes inside this linked list
-myLinkedList.head = node1;
-myLinkedList.head.next = node2;
-myLinkedList.head.next.next = node3;
+// console.log(myLinkedList);
 
-console.log(myLinkedList);
+// // Let's link our nodes inside this linked list
+// myLinkedList.head = node1;
+// myLinkedList.head.next = node2;
+// myLinkedList.head.next.next = node3;
 
-// gets head node
-console.log(myLinkedList.getHeadNode());
+// console.log(myLinkedList);
 
-// traversing
-myLinkedList.traverse();
+// // gets head node
+// console.log(myLinkedList.getHeadNode());
 
-// removing node with value of 7
-// (we aren't necessarily removing it, 
-// we are just reassigning it)
-let walker = myLinkedList.getHeadNode();
-// walker.next = walker.next.next;
+// // traversing
+// myLinkedList.traverse();
 
-// Swapping
-// at the moment we have walker -> next -> next -> null
-//                          4   ->  7   ->  12  -> null                          
-// we are trying to get walker: 4 -> 12 -> 7 -> null
-let temp = walker.next.next; // Node with 12
-walker.next.next = temp.next; // 7 -> null
-temp.next = walker.next; // 12 -> 7
-walker.next = temp; // 4 -> 12 
-// and now we have walker -> next -> next -> null
-//                    4   ->  12   ->  7  -> null   
-console.log(walker);
+// // removing node with value of 7
+// // (we aren't necessarily removing it, 
+// // we are just reassigning it)
+// let walker = myLinkedList.getHeadNode();
+// // walker.next = walker.next.next;
+
+// // Swapping
+// // at the moment we have walker -> next -> next -> null
+// //                          4   ->  7   ->  12  -> null                          
+// // we are trying to get walker: 4 -> 12 -> 7 -> null
+// let temp = walker.next.next; // Node with 12
+// walker.next.next = temp.next; // 7 -> null
+// temp.next = walker.next; // 12 -> 7
+// walker.next = temp; // 4 -> 12 
+// // and now we have walker -> next -> next -> null
+// //                    4   ->  12   ->  7  -> null   
+// console.log(walker);
 
 
-//* Text Node
+// //* Text Node
 
-const textNode = new TextNode("hello world");
-console.log(textNode);
+// const textNode = new TextNode("hello world");
+// console.log(textNode);
 
-//* Element Node
+// //* Element Node
 
-const div = new ElementNode("div");
-div.setTextContent("Hello element node!")
-console.log(div);
+// const div = new ElementNode("div");
+// div.setTextContent("Hello element node!")
+// console.log(div);
 
-//* Test
+// //* Test
 
-const p = document.createElement("p");
-p.textContent = "Hello world"
-console.log(p);
-console.dir(p);
+// const p = document.createElement("p");
+// p.textContent = "Hello world"
+// console.log(p);
+// console.dir(p);
